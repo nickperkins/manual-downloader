67ad92a5b7d7cfc2b78fbd53c2e56dd3d1d2b0b1
Nick Perkins <nick@nickperkins.au>
2024-02-17 12:01:12 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# MA Manual Downloader
+
+## Description
+
+This project is a tool to bulk download PDFs linked to pages in a section of a website. By default, it will download the files that make up the Motorsport Australia manual.
+
+## Installation
+
+This project uses [Poetry](https://python-poetry.org/) as a dependency manager.
+
+### Prerequisites
+
+- Python 3.7 or higher
+- Poetry
+
+If you haven't installed Poetry yet, you can do so by running the following command:
+
+```bash
+curl -sSL https://install.python-poetry.org | python -
+```
+
+## Installing the Project
+
+1. Clone the Project: Clone the project from the repository using git. Replace `your-repository-link` with the link to your repository:
+
+   ```bash
+   git clone your-repository-link
+   ```
+
+2. Navigate to the Project Directory: Change your current directory to the project's directory:
+
+    ```bash
+    cd regs-downloader
+    ```
+
+3. **Install Dependencies**: Install the project dependencies using Poetry:
+
+    ```bash
+    poetry install
+    ```
+
+## Run the Project
+
+You can run the project using Poetry.
+
+```bash
+poetry run manual_downloader
+```
---
+++ manual_downloader/__main__.py
@@ -0,0 +1,116 @@
+import requests
+import os
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+import argparse
+
+
+def download_pdf(url, save_path):
+    """
+    Downloads a PDF file from the given URL and saves it to the specified path.
+
+    Args:
+        url (str): The URL of the PDF file to download.
+        save_path (str): The path where the downloaded PDF file should be saved.
+
+    Returns:
+        None
+    """
+
+    response = requests.get(url)
+    # mkdir if the directory does not exist
+    save_dir = "/".join(save_path.split("/")[:-1])
+    os.makedirs(save_dir, exist_ok=True)
+
+    # save the PDF to the specified path
+
+    with open(save_path, 'wb') as file:
+        file.write(response.content)
+    print(f"PDF downloaded successfully and saved at {save_path}")
+
+
+def get_child_pages(url):
+    """
+    Retrieves the child pages of a given URL recursively.
+
+    Args:
+        url (str): The URL of the main page.
+
+    Returns:
+        list: A list of links that are children of the main page.
+    """
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # find each link in the response
+    child_pages = []
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href and href.startswith(url):
+            child_pages.append(href)
+
+    # recursively call this function for each child page
+    # if the child page is not already in the list
+    for page in child_pages:
+        if page not in child_pages:
+            child_pages.extend(get_child_pages(page))
+
+    # return a list of links that are children of the main page
+    return child_pages
+
+
+def get_pdf_list(child_pages):
+    """
+    Download PDF files from a list of child pages.
+
+    Args:
+        child_pages (list): List of URLs of child pages.
+        save_dir (str): Directory path to save the downloaded PDF files.
+
+    Returns:
+        None
+    """
+    downloads = []
+    for page in child_pages:
+        response = requests.get(page)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            filename = urlparse(href).path.split("/")[-1]
+            # check if the file is a PDF
+            if filename.endswith(".pdf") and href not in downloads:
+                downloads.append(href)
+
+    return downloads
+
+
+def download_pdfs(pdf_list, save_dir):
+    """
+    Download PDF files from a list of URLs.
+
+    Args:
+        pdf_list (list): List of URLs of PDF files to download.
+        save_dir (str): Directory path to save the downloaded PDF files.
+
+    Returns:
+        None
+    """
+    for pdf in pdf_list:
+        filename = urlparse(pdf).path.split("/")[-1]
+        save_path = os.path.join(save_dir, filename)
+        download_pdf(pdf, save_path)
+    print("All PDFs downloaded successfully.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='PDF Grabber for Motorsport Australia Manual.')
+    parser.add_argument('url', type=str, help='Base URL of the manual',
+                        default="https://www.motorsport.org.au/regulations/manual")
+    parser.add_argument('save_dir', type=str, help='Directory path to save the downloaded PDF files',
+                        default=os.path.join(os.getcwd(), "pdfs"))
+    args = parser.parse_args()
+
+    child_pages = get_child_pages(args.url)
+    pdf_list = get_pdf_list(child_pages)
+    download_pdfs(pdf_list, args.save_dir)
